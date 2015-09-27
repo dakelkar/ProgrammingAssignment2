0b7d0fb217fbc2a3791f7a7eeb4384d1acf80949
dakelkar <dakelkar@gmail.com>
2015-09-28 00:03:13 +0530

    Revert "Programming Assignment 2: Lexical Scoping"
    This reverts commit 3f77d07cde2305f0da3b6871e67ea36b803adf49.

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,15 @@
-## the result so that it can be called later
+## Put comments here that give an overall description of what your
+## functions do
 
-## This function takes an input matrix (x) and creates an object with its inverse using the inbuilt function solve
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(solve) m <<- solve
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+
 }
 
 
-## This function takes the output from the makeCacheMatrix function and returns its value
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
 }
